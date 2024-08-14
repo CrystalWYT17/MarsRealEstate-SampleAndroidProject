f94e0f9fe980f9e46f969bb8f33e512a74fff265
CrystalWYT17 <wuttyeetun1721999@gmail.com>
2024-08-14 19:59:44 +0800

    implement retrofit and retrieve data as json

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,11 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+
+//    retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-scalars:2.9.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
